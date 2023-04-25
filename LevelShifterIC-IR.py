06f74b4d0178b578fa9d6a4deb8ab8dd00fa076a
peterpisarz <peter.pisarz@gmail.com>
2023-04-24 22:01:33 -0400

    Level Shifter

---
+++ LevelShifterIC-IR.py
@@ -0,0 +1,31 @@
+import RPi.GPIO as GPIO
+import time
+
+rgb = [13,19,26]
+red = 13
+green = 19
+blue = 26
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(rgb, GPIO.OUT)
+GPIO.setup(21, GPIO.IN)
+GPIO.setup(20, GPIO.IN)
+
+try:
+    while True:
+        if GPIO.input(21) == False:
+            GPIO.output(green, GPIO.LOW)
+            GPIO.output(blue, GPIO.LOW)
+            GPIO.output(red, GPIO.HIGH)
+        elif GPIO.input(20) == False:
+            GPIO.output(green, GPIO.LOW)
+            GPIO.output(red, GPIO.LOW)
+            GPIO.output(blue, GPIO.HIGH)
+        else:
+            GPIO.output(red, GPIO.LOW)
+            GPIO.output(blue, GPIO.LOW)
+            GPIO.output(green, GPIO.HIGH)
+            time.sleep(.1)
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
